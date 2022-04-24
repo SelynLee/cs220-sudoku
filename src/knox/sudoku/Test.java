5de4350feb7eef95fc72a372f4953fe5ffccec49
Selyn <selss@knox.edu>
2022-04-24 15:54:54 -0500

    implemented getLegalValues

--- src/knox/sudoku/Test.java
+++ src/knox/sudoku/Test.java
@@ -4,6 +4,15 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Sudoku sudoku = new Sudoku();
+		sudoku.load("easy1.txt");
+		System.out.println(sudoku);
+		
+		// check a couple of examples
+		// probably someday maybe use JUnit
+		// just not when 40 people are watching you
+		System.out.println(sudoku.getLegalValues(0, 8));
+		System.out.println(sudoku.getLegalValues(6, 8));
 
 	}
 
